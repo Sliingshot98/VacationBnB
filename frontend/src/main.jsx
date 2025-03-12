a21b82eb1f2d6a563e5236340f4a3c23e36d54c9
Slingshot98 <coreycalhoonc@gmail.com>
2025-03-12 01:11:55 -0500

    phase 0

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import configureStore from './store';
+import { Provider } from 'react-redux';
+import configureStore from './store/store.js';
 
 const store = configureStore();
 
-window.store = store;
-
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
