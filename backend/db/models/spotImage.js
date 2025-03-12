5c002038930d8b3bae8d5cb2506a1da760c51b20
Slingshot98 <coreycalhoonc@gmail.com>
2025-03-11 22:34:26 -0500

    fixed an issue

--- backend/db/models/spotImage.js
+++ backend/db/models/spotImage.js
@@ -20,7 +20,7 @@ module.exports = (sequelize, DataTypes) => {
      type: DataTypes.INTEGER,
      allowNull: false,
      references: {
-        model: "Spot"
+        model: "Spots"
      },onDelete: "CASCADE"
    },
    url: {
